a3cc2fe99e8c5c55a7315f39477f86ff52b4a133
probinson2015 <porsha_robinson@ymail.com>
2016-06-02 12:02:52 -0700

    styling

--- js/main.js
+++ js/main.js
@@ -5,15 +5,18 @@ $.getJSON( "https://nuvi-challenge.herokuapp.com/activities", function( data ) {
 	for (var i = 0; i < 20; i++){ //change this to data.length to get all results
 		output += "<li>";
 		output += "<h2><a href='" + data[i].actor_url + "'>" + data[i].actor_name + "</a></h2>";
-		output += "<p> Provider: " + '<span class="tab" id="provider">' + data[i].provider + "</span><a href=''>Respond to Activity</a></p>";
+		output += "<p> Provider: " + '<span class="tab" id="provider">' + data[i].provider + "</span>";
+		output += "<a href='" + data[i].activity_url + "'>Respond to Activity</a></p>";
 		output += "<p> Username: " + data[i].actor_username + "</p>";
 		output += "<img src='" + data[i].actor_avator +"'/>";
+		output += "<span class='info'>";
 		output += "<p> Description: " + data[i].actor_description + "</p>";
 		output += "<p> Activity Message: " + data[i].activity_message + "</p>";
 		var activity_date = new Date(data[i].activity_date).toDateString();
 		output += "<p> Date Posted: " + activity_date + "</p>";
-		output += "<p class='tab'> Likes: " + data[i].activity_likes + "</p><p id='shares'>Shares: " 
-					+ data[i].activity_shares + "</p>";
+		output += "<p class='tab'> Likes: " + data[i].activity_likes + "</p>";
+		output += "<p id='shares'>Shares: " + data[i].activity_shares + "</p>";
+		output += "</span>"
 		output += "</li>";
 	
 
